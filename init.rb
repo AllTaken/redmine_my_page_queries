3ae24140f17c746a21b6b398076e254096bdbf77
AllTaken <AllTaken@users.noreply.github.com>
2015-04-01 23:58:39 +0200

    Updated version number to 0.1.0
    New version

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_my_page_queries do
   name 'MyPage custom queries'
   author 'David Robinson'
   description 'Adds custom queries onto My Page screen\nbased on plugin by Milan Stastny of ALVILA SYSTEMS'
-  version '0.0.1'
+  version '0.1.0'
 
   Rails.configuration.to_prepare do
     MyController.send :include, MyControllerPatch
